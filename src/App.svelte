97b4a8e2458655942bd83e97199366196117161f
yamosan <yamorimasato@gmail.com>
2022-07-12 21:53:19 +0900

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,98 @@
+<script lang="ts">
+  import "modern-css-reset";
+  import logo from "./assets/logo.svg";
+  import CountDownTimer from "./lib/CountDownTimer.svelte";
+
+  const DATETIME_OF_EVENT = new Date("2022-08-19T00:00:00+09:00").getTime();
+
+  const TAKUTEN_2021_URL = "https://www.geikou-takuten.com/2021/";
+</script>
+
+<div class="root">
+  <main>
+    <img src={logo} alt="logo" />
+    <p class="comming_soon">COMING SOON</p>
+    <CountDownTimer date={DATETIME_OF_EVENT} />
+  </main>
+  <footer>
+    <a href={TAKUTEN_2021_URL}>昨年の卓展2021の様子はこちら>></a>
+  </footer>
+</div>
+
+<style>
+  :root {
+    /* font-family: "Noto Sans", sans-serif; */
+    font-family: "Noto Sans", -apple-system, BlinkMacSystemFont, "Segoe UI",
+      Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue",
+      sans-serif;
+  }
+
+  .root {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    min-height: 100vh;
+    position: relative;
+    overflow-x: hidden;
+  }
+
+  main {
+    flex: 1;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    margin-top: -88px;
+  }
+
+  main > img {
+    width: 248px;
+
+    margin-bottom: 32px;
+  }
+
+  main > p {
+    color: #df2b27;
+    font-weight: 700;
+    font-size: min(8.2vw, 71px);
+    letter-spacing: 0.3em;
+    text-indent: 0.4em;
+    line-height: 1;
+
+    margin-bottom: 12px;
+  }
+
+  footer {
+    position: absolute;
+    bottom: 100px;
+  }
+
+  footer > a {
+    text-decoration: underline;
+    color: #df2b27;
+    font-weight: 700;
+    letter-spacing: 0.25em;
+    font-size: 16px;
+  }
+
+  @media screen and (max-width: 480px) {
+    main {
+      margin-top: -110px;
+    }
+
+    main > img {
+      margin-bottom: 42px;
+    }
+
+    main > p {
+      margin-bottom: 14px;
+    }
+
+    footer > a {
+      font-size: 13px;
+    }
+  }
+</style>
---
+++ src/lib/CountDownTimer.svelte
@@ -0,0 +1,89 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  export let date: number; // ms
+
+  let now = Date.now();
+  let interval: NodeJS.Timer;
+
+  $: count = Math.round(date - now) / 1000;
+  $: d = Math.floor(count / 60 / 60 / 24);
+  $: h = Math.floor(count / 60 / 60) % 24;
+  $: m = Math.floor(count / 60) % 60;
+  $: s = Math.floor(count) % 60;
+
+  onMount(() => {
+    interval = setInterval(() => {
+      now = Date.now();
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  });
+
+  const pad2 = (n: number) => {
+    return String(n).padStart(2, "0");
+  };
+
+  $: if (count < 0) clearInterval(interval);
+</script>
+
+<div class="root">
+  <div class="digit">
+    {pad2(d)}
+    <span>days</span>
+  </div>
+  <div class="digit">
+    {pad2(h)}
+    <span>hours</span>
+  </div>
+  <div class="digit">
+    {pad2(m)}
+    <span>mins</span>
+  </div>
+  <div class="digit">
+    {pad2(s)}
+    <span>secs</span>
+  </div>
+</div>
+
+<style>
+  .root {
+    width: 100%;
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    user-select: none;
+    gap: 16px;
+  }
+
+  .digit {
+    display: inline-flex;
+    gap: 5px;
+
+    font-size: min(10vw, 98px);
+    font-weight: 500;
+    font-variant-numeric: tabular-nums;
+    color: #49c0e6;
+    line-height: 1;
+  }
+
+  .digit > span {
+    margin-top: 0.45em;
+
+    font-size: min(4vw, 25px);
+    font-weight: 300;
+  }
+
+  @media screen and (max-width: 480px) {
+    .root {
+      gap: 6px;
+    }
+    .digit {
+      gap: 2px;
+    }
+    .digit > span {
+      margin-top: 0.25em;
+    }
+  }
+</style>
