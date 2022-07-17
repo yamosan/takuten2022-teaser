14a6dfa4c7dcf6afdc4536a13869c28d9e385ad5
yamosan <yamorimasato@gmail.com>
2022-07-18 05:10:41 +0900

    feat: use scss

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,7 +3,9 @@
 
 interface ImportMetaEnv {
   readonly VITE_APP_DATETIME_OF_EVENT: number;
-  readonly VITE_APP_TAKUTEN_2021_URL: string;
+  readonly VITE_APP_TAKUTEN_2021_SITE_URL: string;
+  readonly VITE_APP_TAKUTEN_2021_INSTAGRAM_URL: string;
+  readonly VITE_APP_TAKUTEN_2021_TWITTER_URL: string;
 }
 
 interface ImportMeta {
